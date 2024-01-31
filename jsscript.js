5cfdcde8699021e478faa7fb3386d00af9edb0c8
Marcus Johnson <43221340+MarcusJ27@users.noreply.github.com>
2024-01-30 17:16:15 -0800

    Update jsscript.js

--- jsscript.js
+++ jsscript.js
@@ -1,28 +1,83 @@
-function appendToExpression(value) {
-    document.getElementById("expression").value += value;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const display = document.getElementById("display");
 
-function deleteLast() {
-    let expression = document.getElementById("expression").value;
-    document.getElementById("expression").value = expression.slice(0, -1);
-}
+    document.getElementById("add").onclick = function() {
+        display.value += '+';
+    };
 
-function clearInput() {
-    document.getElementById("expression").value = "";
-    document.getElementById("result").innerText = "";
-}
+    document.getElementById("subtract").onclick = function() {
+        display.value += '-';
+    };
 
-function calculate() {
-    let expression = document.getElementById("expression").value;
-    if (expression) {
-        try {
-            let result = eval(expression);
-            document.getElementById("result").innerText = `${expression} = ${result}`;
-            document.getElementById("expression").value = `${expression} = ${result}`;
-        } catch (error) {
-            document.getElementById("result").innerText = "Error";
-        }
-    }
-}
+    document.getElementById("multiply").onclick = function() {
+        display.value += '*';
+    };
 
+    document.getElementById("divide").onclick = function() {
+        display.value += '/';
+    };
 
+    document.getElementById("openParenthesis").onclick = function() {
+        display.value += '(';
+    };
+
+    document.getElementById("closeParenthesis").onclick = function() {
+        display.value += ')';
+    };
+
+    document.getElementById("zero").onclick = function() {
+        display.value += '0';
+    };
+
+    document.getElementById("one").onclick = function() {
+        display.value += '1';
+    };
+
+    document.getElementById("two").onclick = function() {
+        display.value += '2';
+    };
+
+    document.getElementById("three").onclick = function() {
+        display.value += '3';
+    };
+
+    document.getElementById("four").onclick = function() {
+        display.value += '4';
+    };
+
+    document.getElementById("five").onclick = function() {
+        display.value += '5';
+    };
+
+    document.getElementById("six").onclick = function() {
+        display.value += '6';
+    };
+
+    document.getElementById("seven").onclick = function() {
+        display.value += '7';
+    };
+
+    document.getElementById("eight").onclick = function() {
+        display.value += '8';
+    };
+
+    document.getElementById("nine").onclick = function() {
+        display.value += '9';
+    };
+
+    document.getElementById("decimal").onclick = function() {
+        display.value += '.';
+    };
+
+    document.getElementById("delete").onclick = function() {
+        display.value = display.value.slice(0, -1);
+    };
+
+    document.getElementById("clear").onclick = function() {
+        display.value = '';
+    };
+
+    document.getElementById("equals").onclick = function() {
+        display.value = eval(display.value);
+    };
+});
